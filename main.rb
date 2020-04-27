ca18804dc28469fa400a63b9139cbd5865cbceb7
Nick Blantz <nicholasblantz@gmail.com>
2020-04-26 21:25:17 -0400

    Debugging

--- main.rb
+++ main.rb
@@ -22,8 +22,7 @@ before do
   response.headers['Access-Control-Allow-Origin'] = '*'
 end
 
-get '/scrape_recall/:recall_id' do |recall_id|
+post '/scrape_recall/:recall_id' do |recall_id|
   ScraperWorkerPool::add_job({ msg_type: :REGISTER_RECALL, recall: get_recall(recall_id) })
-  # DatabaseWorkerPool::add_job << { msg_type: :FLAG_POSSIBLE_VIOLATION, recall_id: 23, page_url: 'www.google.com', page_title: 'test' }
-  "scraping recall #{recall_id}"
+  return "scraping recall #{recall_id}"
 end
